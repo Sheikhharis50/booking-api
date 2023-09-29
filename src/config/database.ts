cc8baa14f4db4b8f8891ed0a8256c8e8e429b732
Sheikh Haris Zahid <sheikh.haris.zahid@hotmail.com>
2023-09-29 19:05:37 +0500

    fix: Seeders are written to load initial data with custom scripts.

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,16 +1,28 @@
 import { OrmModuleOptions } from '@/interfaces/ormOptions';
+import { join } from 'path';
+
+/**
+ * The function `getSqliteDBName` returns the name of the SQLite database file based on the environment
+ * and database name.
+ * @returns The function `getSqliteDBName` returns the path to the SQLite database file with the name
+ * "bookings" concatenated with the environment name (if provided) and the file extension ".sqlite3".
+ */
+const getSqliteDBName = () => {
+  let path = __dirname + '/../database';
+  path = join(path, process.env.DATABASE_NAME || 'bookings');
+  path += process.env.NODE_ENV ? `_${process.env.NODE_ENV}` : '';
+  return path + '.sqlite3';
+};
 
 export default (): OrmModuleOptions => ({
   type: 'sqlite',
-  database: `${process.cwd()}/src/database/${
-    process.env.DATABASE_NAME || 'bookings'
-  }.sqlite3`,
-  logging: process.env.DATABASE_LOGGING === 'true',
-  synchronize: process.env.DATABASE_SYNC === 'true',
-  dropSchema: process.env.DATABASE_DROPSCHEMA === 'true',
+  database: getSqliteDBName(),
+  logging: process.env.DATABASE_LOGGING === 'true' || true,
+  synchronize: process.env.DATABASE_SYNC === 'true' || true,
+  dropSchema: process.env.DATABASE_DROPSCHEMA === 'true' || false,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   seeds: [__dirname + '/../database/seeds/**/*{.ts,.js}'],
   factories: [__dirname + '/../database/factories/**/*{.ts,.js}'],
   migrations: [__dirname + '/../database/migrations/**/*{.ts,.js}'],
-  migrationsRun: process.env.DATABASE_MIGRATIONS_RUN === 'true',
+  migrationsRun: process.env.DATABASE_MIGRATIONS_RUN === 'true' || true,
 });
---
+++ src/database/datasource.ts
@@ -0,0 +1,16 @@
+import { DataSource } from 'typeorm';
+import configs from '../config/database';
+
+export default (() => {
+  const dbConfig = { ...configs() };
+  return new DataSource({
+    type: 'sqlite',
+    database: dbConfig.database.toString(),
+    entities: dbConfig.entities,
+    migrations: dbConfig.migrations,
+    logger: 'advanced-console',
+    logging: ['warn', 'error'],
+    dropSchema: true,
+    synchronize: true,
+  });
+})();
